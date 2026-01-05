413e51b96df70daedb5515cfa184d3c76d51784b
chenhai257890 <1614435260@qq.com>
2026-01-05 19:08:22 +0800

    nib load bytes

--- dashboard.py
+++ dashboard.py
@@ -236,7 +236,7 @@ with col1:
         # 读取 NIfTI 文件
         file_bytes = uploaded_file.read()
         file_like_object = io.BytesIO(file_bytes)  # 将字节流转换为 BytesIO 对象
-        nii_image = nib.Nifti1Image.from_bytes(file_like_object)
+        nii_image = nib.Nifti1Image.from_bytes(file_like_object.getvalue())
         img_data = nii_image.get_fdata()  # 获取图像数据
 
         # 获取图像的维度
